cdb7b5ae2d1b869556c3b02345385498df2d1f33
canpok1 <6isstrong@gmail.com>
2015-04-12 14:56:17 +0900

    GameManagerのテストを追加

---
+++ ReversiTest/src/core/GameManagerTest.java
@@ -0,0 +1,78 @@
+// CHECKSTYLE:OFF
+
+package core;
+
+import mockit.Mocked;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.experimental.runners.Enclosed;
+import org.junit.runner.RunWith;
+
+import output.GameViewer;
+import ai.GamePlayer;
+
+/**
+ * {@link GameManager}の単体テストです.
+ * @author tanabe
+ *
+ */
+@RunWith(Enclosed.class)
+public class GameManagerTest {
+
+    public static class インスタンス化テスト {
+        
+        @Test(expected = IllegalArgumentException.class)
+        public void 第一引数がnullだと例外発生(
+                @Mocked GamePlayer mockPlayer,
+                @Mocked Board mockBoard,
+                @Mocked GameViewer mockViewer) {
+            new GameManager(null, mockPlayer, mockBoard, mockViewer);
+        }
+        
+        @Test(expected = IllegalArgumentException.class)
+        public void 第二引数がnullだと例外発生(
+                @Mocked GamePlayer mockPlayer,
+                @Mocked Board mockBoard,
+                @Mocked GameViewer mockViewer) {
+            new GameManager(mockPlayer, null, mockBoard, mockViewer);
+        }
+        
+        @Test(expected = IllegalArgumentException.class)
+        public void 第三引数がnullだと例外発生(
+                @Mocked GamePlayer mockPlayer1,
+                @Mocked GamePlayer mockPlayer2,
+                @Mocked GameViewer mockViewer) {
+            new GameManager(mockPlayer1, mockPlayer2, null, mockViewer);
+        }
+        
+        @Test(expected = IllegalArgumentException.class)
+        public void 第四引数がnullだと例外発生(
+                @Mocked GamePlayer mockPlayer1,
+                @Mocked GamePlayer mockPlayer2,
+                @Mocked Board mockBoard) {
+            new GameManager(mockPlayer1, mockPlayer2, mockBoard, null);
+        }
+        
+    }
+    
+    public static class ゲーム終了テスト {
+        
+        @Test
+        public void isFinishがインスタンス化直後にTrueを返すか(
+                @Mocked GamePlayer mockPlayer1,
+                @Mocked GamePlayer mockPlayer2,
+                @Mocked Board mockBoard,
+                @Mocked GameViewer mockViewer) {
+            GameManager manager
+                = new GameManager(
+                    mockPlayer1,
+                    mockPlayer2,
+                    mockBoard,
+                    mockViewer);
+            Assert.assertTrue(manager.isFinish());
+        }
+    }
+}
+
+//CHECKSTYLE:ON
